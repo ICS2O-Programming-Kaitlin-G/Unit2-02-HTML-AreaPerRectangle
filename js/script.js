d35b4e69a94a19226b28a31bae883ca5df638b90
Katie <99366154+kaitlingreen@users.noreply.github.com>
2022-03-24 02:02:37 +0000

    hello github! look at my website! do you like it? i think it's pretty funky, personally.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Created by: Katie
+// Created on: Mar 2022
+// This file has the JS functions for index.html
+
+function findMathClicked() {
+  // This function does the area and perimeter equation
+
+  document.getElementById('find-area').innerHTML = 'The area is ' + (16*22) + 'cm²'
+	document.getElementById('find-per').innerHTML = 'The perimeter is ' + (16*22 + 16*22) + 'cm'
+  alert("how could you cheat. now my friend will cry. reflect on your actions. are you proud of this course of action? you've just made a teenager cry. a TEENAGER. how could you. I hope you have fun with your math answers now. You better say thank you to the beautiful javascript function that gave you those numbers.")
+}
